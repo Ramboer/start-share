fe908adbb500ed8804ea5a5d6848863d7be6015e
simonliu007 <simon.liu@intelligentgroup.cn>
2017-01-04 16:06:27 +0800

    share commit.

---
+++ share/src/main/java/share.java
@@ -0,0 +1,12 @@
+/**
+ * Created by simon.liu on 2017/1/4.
+ */
+public class share {
+    /**
+     *
+     * @param args String[]
+     */
+    public static void main(String[] args) {
+        System.out.println("start to learn... share first");
+    }
+}
